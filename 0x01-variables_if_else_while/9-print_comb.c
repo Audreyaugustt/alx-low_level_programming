79344dda3a72e2e6a62069531a46b77f415eceb1
Audreyaugustt <audreyaugustt@gmail.com>
2023-12-18 11:34:37 -0800

    Prints all possible combinations of single-digit numbers, with coma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of a program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
